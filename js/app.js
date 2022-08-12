82edd5f5b5ee5e5d9b1c80e4a935729aa8a63116
Luciano <lucasacci@hotmail.com>
2022-08-11 22:46:54 -0300

    practica dom

--- js/app.js
+++ js/app.js
@@ -11,24 +11,44 @@ function cambiarTitulo(){
 }
 
 function verMas(){
-    console.log(`prueba ver mas`);
-
-    let parrafoNuevo = document.createElement('p');
-    parrafoNuevo.innerHTML = 'lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ullam dolorum eum earum suscipit, voluptas recusandae voluptatem dolorem nisi doloribus, rerum sunt eius maxime error officiis doloremque dolore provident voluptates incidunt saepe dolor asperiores exercitationem sequi. Ullam numquam ab unde facere enim consequuntur aliquid, magnam labore! Magnam omnis accusantium accusamus.orem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ullam dolorum eum earum suscipit, voluptas recusandae voluptatem dolorem nisi doloribus, rerum sunt eius maxime error officiis doloremque dolore provident voluptates incidunt saepe dolor asperiores exercitationem sequi. Ullam numquam ab unde facere enim consequuntur aliquid, magnam labore! Magnam omnis accusantium accusamus.';
-
-    parrafoNuevo.className = 'lead';
-
-    console.log(parrafoNuevo)
-
+    let botonVerMas = document.getElementById('btnVerMas');
     let sectionPadre = document.getElementsByClassName('container');
-    console.log(sectionPadre[1]);
-    // sectionPadre[1].appendChild(parrafoNuevo);
-    // sectionPadre[1].prepend(parrafoNuevo);
-    sectionPadre[1].insertBefore(parrafoNuevo, document.getElementById('btnVerMas'));
 
-    //para acceder a lo que escribo dentro de un imput NO USAR INNER.HTML usar VALUE
+    if(botonVerMas.innerHTML === 'Ver mas...'){
 
-    console.log(document.getElementById('buscador').value);
+        let parrafoNuevo = document.createElement('p');
+        parrafoNuevo.innerHTML = 'lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ullam dolorum eum earum suscipit, voluptas recusandae voluptatem dolorem nisi doloribus, rerum sunt eius maxime error officiis doloremque dolore provident voluptates incidunt saepe dolor asperiores exercitationem sequi. Ullam numquam ab unde facere enim consequuntur aliquid, magnam labore! Magnam omnis accusantium accusamus.orem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ullam dolorum eum earum suscipit, voluptas recusandae voluptatem dolorem nisi doloribus, rerum sunt eius maxime error officiis doloremque dolore provident voluptates incidunt saepe dolor asperiores exercitationem sequi. Ullam numquam ab unde facere enim consequuntur aliquid, magnam labore! Magnam omnis accusantium accusamus.';
+    
+        parrafoNuevo.className = 'lead';
+    
+        console.log(parrafoNuevo)
+    
+        console.log(sectionPadre[1]);
+        // sectionPadre[1].appendChild(parrafoNuevo); // agrega hijo al ultimo
+        // sectionPadre[1].prepend(parrafoNuevo); // agrega hijo al principio
+        sectionPadre[1].insertBefore(parrafoNuevo, botonVerMas);
+        
+        //cambiar texto del boton
+
+
+        botonVerMas.className = 'btn btn-outline-danger'
+        botonVerMas.innerHTML = 'Ocultar'
+    
+        
+        //para acceder a lo que escribo dentro de un imput NO USAR INNER.HTML usar VALUE
+    
+        console.log(document.getElementById('buscador').value);
+    }
+    else{
+        // eliminar el parrafor creado
+
+        sectionPadre[1].removeChild(sectionPadre[1].children[2]);
+        console.log(sectionPadre[1].children[2]);
+
+        botonVerMas.innerHTML = 'Ver mas...'
+        botonVerMas.className = 'btn btn-outline-primary'
+    }
+
 
 
 
